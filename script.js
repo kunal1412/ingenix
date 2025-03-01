cd209a2ae0f9bd3a9a041459896bbe8038bc2f07
Kunal1412 <mahajankunal143km@gmail.com>
2025-03-01 10:18:43 +0530

    28 Feb

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-// Smooth Scroll Effect
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
+document.addEventListener('mousemove', (e) => {
+  const triangles = document.querySelector('.glowing-triangles');
+  const x = e.clientX / window.innerWidth;
+  const y = e.clientY / window.innerHeight;
+
+  triangles.style.transform = `translate(${x * 20}px, ${y * 20}px)`;
+});
+
+// backgroung Img
+document.addEventListener("DOMContentLoaded", function() {
+  const images = document.querySelectorAll('.hero-image');
+  images.forEach((img, index) => {
+    img.style.animationDelay = `${index * 2}s`;
   });
+});
